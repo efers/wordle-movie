3e1a7c3cf6089bf418c4b5b9184a5a07ce2fcebb
bhavesh15184 <vipul15184@gmail.com>
2022-02-05 21:55:11 +0530

    messages and game info changes

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -11,12 +11,24 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
       <p className="text-sm text-gray-500 dark:text-gray-300">
         This is an open source word guessing game -{' '}
         <a
-          href="https://github.com/hannahcode/GAME"
+          href="https://github.com/bhavesh15184/word-guessing-game"
           className="underline font-bold"
+          target="_blank" 
         >
           check out the code here
         </a>{' '}
       </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Its forked from the original wordle game -{' '}
+        <a
+          href="https://github.com/hannahcode/word-guessing-game"
+          className="underline font-bold"
+          target="_blank" 
+        >
+          check out the original code here
+        </a>{' '}
+      </p>
+      
     </BaseModal>
   )
 }
